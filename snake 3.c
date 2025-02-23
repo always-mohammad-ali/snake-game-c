4a65431fd869f593ee14aa3ea8061d5f83dead0d
Aethon <afifahmed58@gmail.com>
2025-02-24 01:01:46 +0600

    Add files via upload

---
+++ snake 3.c
@@ -0,0 +1,12 @@
+void logic(){
+
+//snake tail movement and head movement with arrays(complete later)
+
+
+//colision detection(complete)
+if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+        gameOver = 1;
+
+   //logic for game over if tail hit is detected(unfinished)
+}
+
