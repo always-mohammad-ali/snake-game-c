05451d0a1d9660bbfe72a7c632890796a85245f5
Farden10 <farden.hossain.251@northsouth.edu>
2025-03-27 02:18:59 +0600

    Update snakegame part 1.c
    working on the main  gam loop,defined the fruit and position and cursor changes

--- snakegame part 1.c
+++ snakegame part 1.c
@@ -5,14 +5,38 @@
 #include<windows.h>
 #include<stdlib.h>
 #include<time.h>
-
- // GAME CONFIGURATION CONSTANTS//
- # define WIDTH 30
- # define HEIGHT 20
- # define SPEED 200
-
-//GLOBAL GAME STATE VARIABLES
+#include<string.h>
+ # define WIDTH 50
+ # define HEIGHT 30
+ # define SPEED 300
 
 int gameover,score;
 int x ,y;//snake head position
-int fruitX,fruitY;// food position
+int fruitx,fruity;//fruit position at x and y axis
+int tailx[100],taily[100];
+int atail;//tail length
+enum eDirection {STOP=0,LEFT,RIGHT,UP,DOWN};
+enum eDIRECTION dir;
+
+void Setup(){
+gameover=0;
+dir=STOP;
+x=WIDTH/2;
+y=LENGTH/2;
+fruitx=rand()% WIDTH;
+fruity=rand()%HEIGHT;
+score=0;
+atail=0;}
+
+void RunGame(){
+srand(time(0));
+Setup();
+CONSOLE_CURSOR_INFO
+cursorInfo;
+GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursorinfo);
+
+
+}
+
+
+
