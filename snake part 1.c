a49a2cc908eb55bdcfe3f4155c00a503a90daae1
Farden10 <farden.hossain.251@northsouth.edu>
2025-04-15 21:35:24 +0600

    Update snake part 1.c

--- snake part 1.c
+++ snake part 1.c
@@ -5,14 +5,46 @@
 #include<windows.h>
 #include<stdlib.h>
 #include<time.h>
-
- // GAME CONFIGURATION CONSTANTS//
+#include<string.h>
  # define WIDTH 30
  # define HEIGHT 20
  # define SPEED 200
 
-//GLOBAL GAME STATE VARIABLES
-
 int gameover,score;
 int x ,y;//snake head position
-int fruitX,fruitY;// food position
+int fruitx,fruity;//fruit position at x and y axis
+int tailx[100],taily[100];
+int atail;//tail length
+enum eDirection {STOP=0,LEFT,RIGHT,UP,DOWN};
+enum eDIRECTION dir;
+
+void Setup(){
+gameover=0;
+dir=STOP;
+x=WIDTH/2;
+y=LENGTH/2;
+fruitx=rand()% WIDTH;
+fruity=rand()%HEIGHT;
+score=0;
+atail=0;}
+
+void RunGame(){
+srand(time(0));
+Setup();
+CONSOLE_CURSOR_INFO
+cursorInfo;
+GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE),&cursorinfo);
+ while(!gameOver){
+  Draw();
+  Input();
+  Logic();
+  Sleep(SPEED);
+  system("cls");
+  printf("Game Over!Final Score:%d\n",score);
+  printf("Press any key to exit....");
+  getch();
+
+
+}
+
+
