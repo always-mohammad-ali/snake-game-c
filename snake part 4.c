7ee4ab8168400cfe9b151b80a52a9ce248f6d27d
Mohammad Ali <86775213+always-mohammad-ali@users.noreply.github.com>
2025-04-15 20:59:19 +0600

    Update snake part 4.c
    This is the final updated part of my project.

--- snake part 4.c
+++ snake part 4.c
@@ -1,18 +1,57 @@
 #include <stdio.h>
+#include <windows.h>
 
 int main(){
-void gotoxy(int x, int y) {
+//4. RENDERING SYSTEM (Mohammad Ali)
+
+void gotoxy(int x, int y)
+{
     COORD coord = {x, y};
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
-void Draw() {
-    gotoxy(0, 0); // Reset cursor position
+void Draw()
+{
+    gotoxy(0, 0);
 
-    // Draw top border
+
     printf(" ");
     for (int i = 0; i < WIDTH + 2; i++) printf("#");
     printf("\n");
 
+
+    for (int i = 0; i < HEIGHT; i++)
+    {
+        printf(" #");
+        for (int j = 0; j < WIDTH; j++)
+        {
+
+            if (i == y && j == x) printf("O");
+
+            else if (i == fruitY && j == fruitX) printf("F");
+
+            else
+            {
+                int isTail = 0;
+                for (int k = 0; k < nTail; k++)
+                {
+                    if (tailX[k] == j && tailY[k] == i)
+                    {
+                        printf("o");
+                        isTail = 1;
+                        break;
+                    }
+                }
+                if (!isTail) printf(" ");
+            }
+        }
+        printf("# \n");
+    }
+
+
+    printf(" ");
+    for (int i = 0; i < WIDTH + 2; i++) printf("#");
+    printf("\nScore: %d", score);
+}
     return 0;
 }
