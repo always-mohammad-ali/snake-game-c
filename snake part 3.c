0b804ef4df98ed0f9b4454474356773efe7ac880
Aethon <afifahmed58@gmail.com>
2025-04-16 01:19:52 +0600

    Update snake part 3.c

--- snake part 3.c
+++ snake part 3.c
@@ -1,12 +1,54 @@
-void logic(){
+void Logic()
+{
+    
+    int prevX = tailX[0];
+    int prevY = tailY[0];
+    tailX[0] = x;
+    tailY[0] = y;
 
-//snake tail movement and head movement with arrays(complete later)
+    for (int i = 1; i < nTail; i++)
+    {
+        int tempX = tailX[i];
+        int tempY = tailY[i];
+        tailX[i] = prevX;
+        tailY[i] = prevY;
+        prevX = tempX;
+        prevY = tempY;
+    }
 
+   
+    switch (dir)
+    {
+    case LEFT:
+        x--;
+        break;
+    case RIGHT:
+        x++;
+        break;
+    case UP:
+        y--;
+        break;
+    case DOWN:
+        y++;
+        break;
+    }
 
-//colision detection(complete)
-if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
+    
+    if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
         gameOver = 1;
 
-   //logic for game over if tail hit is detected(unfinished)
-}
+    for (int i = 0; i < nTail; i++)
+    {
+        if (tailX[i] == x && tailY[i] == y)
+            gameOver = 1;
+    }
 
+    
+    if (x == fruitX && y == fruitY)
+    {
+        score += 10;
+        fruitX = rand() % WIDTH;
+        fruitY = rand() % HEIGHT;
+        nTail++;
+    }
+}
