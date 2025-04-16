15acaf954020efefa36a32d4cd5a602318ba2771
Jayonti191106 <69365504+Jayonti191106@users.noreply.github.com>
2025-04-16 06:15:33 +0600

    last commit before final

--- main.c
+++ main.c
@@ -1,27 +1,26 @@
-
-//1. GAME INITIALIZATION & MAIN LOOP (Farden Hossain)
-
 #include <stdio.h>
 #include <conio.h>
 #include <windows.h>
 #include <stdlib.h>
 #include <time.h>
-
-
-#define WIDTH 30
+#define WIDTH 80
 #define HEIGHT 20
 #define SPEED 200
 
-
-int gameOver, score;
-int x, y;           
-int fruitX, fruitY; 
-int tailX[100], tailY[100]; 
-int nTail;          
+int gameOver, score, x, y, fruitX, fruitY, tailX[100], tailY[100], nTail;
 enum eDirection { STOP = 0, LEFT, RIGHT, UP, DOWN };
 enum eDirection dir;
 
+void Setup();
+void RunGame();
+void Input();
+void Logic();
+void gotoxy(int, int);
+void Draw();
+void GenerateFood();
+void UpdateScore(int);
 
+// 1. GAME INITIALIZATION & MAIN LOOP (Farden Hossain)
 void Setup()
 {
     gameOver = 0;
@@ -33,14 +32,11 @@ void Setup()
     score = 0;
     nTail = 0;
 }
-
-
 void RunGame()
 {
     srand(time(0));
     Setup();
 
-    
     CONSOLE_CURSOR_INFO cursorInfo;
     GetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursorInfo);
     cursorInfo.bVisible = 0;
@@ -48,54 +44,47 @@ void RunGame()
 
     while (!gameOver)
     {
-        
         Draw();
         Input();
         Logic();
         Sleep(SPEED);
     }
 
-    
     system("cls");
     printf("Game Over! Final Score: %d\n", score);
     printf("Press any key to exit...");
     _getch();
 }
 
-
-//2. INPUT HANDLING (Jayonti Sarkar)
-
+// 2. INPUT HANDLING (Jayonti Sarkar)
 void Input()
 {
-    if (_kbhit())    
+    if (_kbhit())
     {
         switch (_getch())
         {
-        case 'a':
-            if (dir != RIGHT) dir = LEFT;
-            break;
-        case 'd':
-            if (dir != LEFT) dir = RIGHT;
-            break;
-        case 'w':
-            if (dir != DOWN) dir = UP;
-            break;
-        case 's':
-            if (dir != UP) dir = DOWN;
-            break;
-        case 'x':
-            gameOver = 1;
-            break;
+            case 'a':
+                if (dir != RIGHT) dir = LEFT;
+                break;
+            case 'd':
+                if (dir != LEFT) dir = RIGHT;
+                break;
+            case 'w':
+                if (dir != DOWN) dir = UP;
+                break;
+            case 's':
+                if (dir != UP) dir = DOWN;
+                break;
+            case 'x':
+                gameOver = 1;
+                break;
         }
     }
 }
 
-
-//3. SNAKE MECHANICS (Afif Chowdhury)
-
+// 3. SNAKE MECHANICS (Afif Chowdhury)
 void Logic()
 {
-    
     int prevX = tailX[0];
     int prevY = tailY[0];
     tailX[0] = x;
@@ -110,35 +99,25 @@ void Logic()
         prevX = tempX;
         prevY = tempY;
     }
-
-   
     switch (dir)
     {
-    case LEFT:
-        x--;
-        break;
-    case RIGHT:
-        x++;
-        break;
-    case UP:
-        y--;
-        break;
-    case DOWN:
-        y++;
-        break;
+        case LEFT:  x--; break;
+        case RIGHT: x++; break;
+        case UP:    y--; break;
+        case DOWN:  y++; break;
+        default: break;
     }
-
-    
     if (x < 0 || x >= WIDTH || y < 0 || y >= HEIGHT)
         gameOver = 1;
 
     for (int i = 0; i < nTail; i++)
     {
         if (tailX[i] == x && tailY[i] == y)
+        {
             gameOver = 1;
+            break;
+        }
     }
-
-    
     if (x == fruitX && y == fruitY)
     {
         score += 10;
@@ -148,35 +127,31 @@ void Logic()
     }
 }
 
-
-//4. RENDERING SYSTEM (Mohammad Ali)
-
+// 4. RENDERING SYSTEM (Mohammad Ali)
 void gotoxy(int x, int y)
 {
-    COORD coord = {x, y};
+    COORD coord = { x, y };
     SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
 }
 
 void Draw()
 {
-    gotoxy(0, 0); 
-
-    
+    gotoxy(0, 0);
     printf(" ");
-    for (int i = 0; i < WIDTH + 2; i++) printf("#");
+    for (int i = 0; i < WIDTH + 2; i++)
+        printf("#");
     printf("\n");
 
-   
+
     for (int i = 0; i < HEIGHT; i++)
     {
         printf(" #");
         for (int j = 0; j < WIDTH; j++)
         {
-           
-            if (i == y && j == x) printf("O");
-           
-            else if (i == fruitY && j == fruitX) printf("F");
-           
+            if (i == y && j == x)
+                printf("O");
+            else if (i == fruitY && j == fruitX)
+                printf("F");
             else
             {
                 int isTail = 0;
@@ -189,21 +164,21 @@ void Draw()
                         break;
                     }
                 }
-                if (!isTail) printf(" ");
+                if (!isTail)
+                    printf(" ");
             }
         }
-        printf("# \n");
+        printf("#\n");
     }
 
-   
+
     printf(" ");
-    for (int i = 0; i < WIDTH + 2; i++) printf("#");
+    for (int i = 0; i < WIDTH + 2; i++)
+        printf("#");
     printf("\nScore: %d", score);
 }
 
-
-//5. FOOD & SCORE SYSTEM (MD. Nahim)
-
+// 5. FOOD & SCORE SYSTEM (MD. Nahim)
 void GenerateFood()
 {
     fruitX = rand() % WIDTH;
